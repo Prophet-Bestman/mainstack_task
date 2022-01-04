c14067b9363cb973e9f02273e7d06dbd5434e689
Prophet-Bestman <bestiproph@gmail.com>
2022-01-05 00:30:54 +0100

    Minor refactor on padding

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -3,11 +3,11 @@ import React from "react";
 const Conversations = () => {
   return (
     <div className="min-h-[1024px] bg-brand-bg px-0 sm:px-7 md:px-[100px]">
-      <div className="min-h-[822px] rounded-[30px] px-[72px] pt-[66px] pb-[76px] bg-gradient-to-r from-brand-darkGradientFrom  to-brand-darkGradienttO flex items-center">
+      <div className="min-h-[822px] rounded-[30px] px-4 md:px-[72px] pt-[66px] pb-[76px] bg-gradient-to-r from-brand-darkGradientFrom  to-brand-darkGradienttO flex items-center">
         <div className="flex flex-col md:grid grid-cols-2 xl:grid-cols-3 gap-14 h-full w-full">
           <div className="col-span-2 xl:col-span-1 flex flex-col lg:flex-row xl:flex-col h-full ">
             <div className="max-w-[400px]">
-              <h2 className="text-2xl md:text-[40px] md:leading-[52px] tracking-[0.02rem] font-bold text-white">
+              <h2 className="text-3xl md:text-[40px] md:leading-[52px] tracking-[0.02rem] font-bold text-white">
                 Conversations designed for people, not bots.
               </h2>
               <p className="text-brand-gray font-medium text-base md:text-xl mt-6 md:leading-[150%]">
